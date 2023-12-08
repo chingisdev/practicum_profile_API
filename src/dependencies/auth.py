41ce749dd9d26d956220a5b60faaca6ec7bc78bf
Chingis <chingisdev@gmail.com>
2023-12-08 16:57:54 +0300

    Add user extraction from auth

---
+++ src/dependencies/auth.py
@@ -0,0 +1,17 @@
+from fastapi import Request
+
+from core.settings import settings
+from models import User
+
+
+def get_user_from_request_state(request: Request) -> User:
+    authorized_user: User = User(
+        id='2f384bd0-97a7-45e1-9f9f-da79affa8048',
+        first_name='test',
+        last_name='test',
+        email='test',
+        is_admin=True,
+    )
+    if settings.auth_enabled:
+        authorized_user = request.state.user
+    return authorized_user
