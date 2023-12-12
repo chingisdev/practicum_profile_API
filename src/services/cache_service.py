f647ea8580aff30fc46c8a28e88531ad66dbea9c
Chingis <chingisdev@gmail.com>
2023-12-13 00:03:41 +0300

    Delete serializer from cache service

--- src/services/cache_service.py
+++ src/services/cache_service.py
@@ -1,35 +1,27 @@
-from typing import Any, Dict, List, Optional, Protocol
+import json
+from typing import Any, Dict, List, Optional
 
 from redis.asyncio import Redis
 
 from utils.backoff import backoff_public_methods
 
 
-class SerializerProtocol(Protocol):
-    def dumps(self, to_convert: Any) -> Any:
-        raise NotImplementedError
-
-    def loads(self, to_convert: Any) -> Any:
-        raise NotImplementedError
-
-
 @backoff_public_methods()
 class CacheService:
-    def __init__(self, redis_client: Redis, serializer: SerializerProtocol, ttl: int = 300):
+    def __init__(self, redis_client: Redis, ttl: int = 300):
         self.redis_client = redis_client
-        self.serializer = serializer
         self.ttl = ttl
 
     async def store_single(self, key: str, to_store: Any, ttl: Optional[int] = None) -> None:
         expiration_shift = ttl or self.ttl
-        serialized_value = self.serializer.dumps(to_store)
+        serialized_value = json.dumps(to_store)
         await self.redis_client.set(key, serialized_value, ex=expiration_shift)
 
     async def store_many(self, to_store: Dict[str, Any], ttl: Optional[int] = None) -> None:
         expiration_shift = ttl or self.ttl
         async with self.redis_client.pipeline() as pipe:
             for key, storable in to_store.items():
-                serialized_value = self.serializer.dumps(storable)
+                serialized_value = json.dumps(storable)
                 await pipe.set(key, serialized_value, ex=expiration_shift)
             await pipe.execute()
 
@@ -37,7 +29,7 @@ class CacheService:
         extractable = await self.redis_client.get(key)
         if extractable:
             await self.redis_client.expire(key, self.ttl)
-            return self.serializer.loads(extractable)
+            return extractable
         return None
 
     async def get_many(self, keys: List[str]) -> Dict[str, Any]:
@@ -50,6 +42,5 @@ class CacheService:
         for key, extractable in zip(keys, to_extract):
             if extractable:
                 await self.redis_client.expire(key, self.ttl)
-                key_extractable_dictionary[key] = self.serializer.loads(extractable)
-
+                key_extractable_dictionary[key] = extractable
         return key_extractable_dictionary
