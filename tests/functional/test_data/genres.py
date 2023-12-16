eec24134e679e82067435a7a84523a3e52eb24e5
Alexander Lyotov <alexander.lyotov@yandex.ru>
2023-12-17 00:19:04 +0300

    feature/fake  данные для жанров

---
+++ tests/functional/test_data/genres.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+COMEDY = {
+    'name': 'Comedy',
+    'description': 'Funny film',
+    'created_at': datetime(2023, 12, 17),
+    'updated_at': datetime(2023, 12, 17),
+}
