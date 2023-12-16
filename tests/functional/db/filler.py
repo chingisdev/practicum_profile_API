44b816d41860c15e785a6f9109da2d3f292bfc7a
Alexander Lyotov <alexander.lyotov@yandex.ru>
2023-12-17 00:17:52 +0300

    feature/функция для заполнения бд

---
+++ tests/functional/db/filler.py
@@ -0,0 +1,22 @@
+"""Утилита для добавления записей в таблицу базы данных."""
+from typing import Any
+
+from src.models.base import Base
+
+
+async def add_record(pg_session, model, filler: dict[str, Any]) -> Base:
+    """Добавить запись в таблицу.
+
+    Args:
+        pg_session: асинхронная сессия sqlalchemy;
+        model: модель в которую нужно добавить запись;
+        filler: данные, для создания записи.
+
+    Returns:
+        Base: созданная запись.
+    """
+    new_record = model(**filler)
+    pg_session.add(new_record)
+    await pg_session.commit()
+    await pg_session.refresh(new_record)
+    return new_record
