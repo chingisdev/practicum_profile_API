79bca9e29e712f689e573a8aae5f66cebceaf4e7
Alexander Lyotov <alexander.lyotov@yandex.ru>
2023-12-17 00:18:30 +0300

    feature/fake данные кинопроизведений

---
+++ tests/functional/test_data/filmworks.py
@@ -0,0 +1,13 @@
+"""Данные для заполнения таблицы кинопроизведений в тестах."""
+from datetime import datetime
+
+RNR_HIGHSCHOOL = {
+    'title': "Rock'n'Roll Highschool",
+    'description': 'American musical comedy film',
+    'file_path': 'E:/cinema/rnr_highschool.mp4',
+    'rating': 4.0,
+    'type': 'fail',
+    'creation_date': datetime(1979, 9, 1),
+    'created_at': datetime(2023, 12, 17),
+    'updated_at': datetime(2023, 12, 17),
+}
