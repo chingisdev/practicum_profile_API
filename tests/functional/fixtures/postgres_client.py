2d94a9e24a52d87f2cc22e9474f2428fbf545754
Alexander Lyotov <alexander.lyotov@yandex.ru>
2023-12-17 18:46:08 +0300

    feature/атрибут base_model AsyncSimplePostgresClient

--- tests/functional/fixtures/postgres_client.py
+++ tests/functional/fixtures/postgres_client.py
@@ -6,17 +6,21 @@ from sqlalchemy import event
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.schema import CreateSchema, DropSchema
 
-from tests.functional.models.base import Base
+from tests.functional.models.auth.base import Base as Auth_Base
+from tests.functional.models.movies.base import Base as Movies_Base
 from tests.functional.settings import test_settings
 
-event.listen(Base.metadata, 'before_create', CreateSchema('content'))
-event.listen(Base.metadata, 'after_drop', DropSchema('content', cascade=True))
+event.listen(Auth_Base.metadata, 'before_create', CreateSchema('auth'))
+event.listen(Auth_Base.metadata, 'after_drop', DropSchema('auth', cascade=True))
+
+event.listen(Movies_Base.metadata, 'before_create', CreateSchema('content'))
+event.listen(Movies_Base.metadata, 'after_drop', DropSchema('content', cascade=True))
 
 
 class AsyncSimplePostgresClient:
     """Класс упрощённого асинхронного клиента базы данных Postgres."""
 
-    def __init__(self, url: str):
+    def __init__(self, url: str, base_model):
         """Инициализация экземпляра упрощённого для тестов клиента базы данных Postgres.
 
         Args:
@@ -34,16 +38,17 @@ class AsyncSimplePostgresClient:
             class_=AsyncSession,
             expire_on_commit=False,
         )
+        self.base_model = base_model
 
     async def create_database(self) -> None:
         """Метод для создания базы данных Postgres."""
         async with self.engine.begin() as conn:
-            await conn.run_sync(Base.metadata.create_all)
+            await conn.run_sync(self.base_model.metadata.create_all)
 
     async def purge_database(self) -> None:
         """Метод для удаления базы данных Postgres."""
         async with self.engine.begin() as conn:
-            await conn.run_sync(Base.metadata.drop_all)
+            await conn.run_sync(self.base_model.metadata.drop_all)
 
 
 @pytest_asyncio.fixture(scope='function')  # Тесты не должны влиять друг на друга
@@ -83,6 +88,7 @@ async def movies_api_pg_session_auth():
             port=test_settings.movies_postgres_port,
             db_name=test_settings.movies_postgres_db,
         ),
+        base_model=Movies_Base,
     )
     await postgres_client.create_database()
     async with postgres_client.async_session() as session:
@@ -100,5 +106,6 @@ if __name__ == '__main__':
             port=test_settings.POSTGRES_PORT,
             db_name=test_settings.POSTGRES_DB,
         ),
+        base_model=Auth_Base,
     )
     asyncio.run(postgres_client.purge_database())
