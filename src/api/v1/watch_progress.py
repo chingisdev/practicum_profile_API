0fb73a294a0e968caac245b52777007831294440
Chingis <chingisdev@gmail.com>
2023-12-19 00:17:37 +0300

    Extract logic from watch progress router

--- src/api/v1/watch_progress.py
+++ src/api/v1/watch_progress.py
@@ -1,12 +1,8 @@
-from aiokafka import AIOKafkaProducer  # type: ignore
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
 
-from src.core.settings import settings
-from src.db_models.watch_progress import WatchProgressModel
 from src.dependencies.auth import get_user_from_request_state
-from src.dependencies.kafka import get_kafka_producer
-from src.endpoint_services.watch_progress import get_watch_progress_model
+from src.endpoint_services.watch_progress import WatchProgressUgcHandler, get_watch_progress_ugc_service
 from src.models.movie_progress import MovieProgress
 from src.models.user import User
 
@@ -21,17 +17,10 @@ router = APIRouter()
 async def handle_user_view_progress(
     movie: MovieProgress,
     user: User = Depends(get_user_from_request_state),
-    kafka_producer: AIOKafkaProducer = Depends(get_kafka_producer),
-    collection: WatchProgressModel = Depends(get_watch_progress_model),
+    progress_ugc_service: WatchProgressUgcHandler = Depends(get_watch_progress_ugc_service),
 ) -> JSONResponse:
     try:
-        await collection.update_progress(user_id=user.id, movie_id=movie.id, break_point=movie.break_point)
-        combined_data = {'movie': movie.model_dump(), 'user': user.model_dump()}
-        key = '{movie}+{user}'.format(movie=movie.id, user=user.id)
-        await kafka_producer.send(
-            topic=settings.watch_progress_topic, key=key, value=combined_data,
-        )
-
+        await progress_ugc_service.update(movie_progress=movie, user=user)
         return JSONResponse(
             status_code=status.HTTP_202_ACCEPTED,
             content={'detail': "User's watch progress has been saved"},
