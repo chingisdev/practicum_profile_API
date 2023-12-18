8ca722af24071f0c33202142426c45f6d8c02441
Chingis <chingisdev@gmail.com>
2023-12-18 23:33:51 +0300

    Change expected type of review to str

--- src/db_models/review.py
+++ src/db_models/review.py
@@ -4,7 +4,6 @@ from motor.core import AgnosticDatabase
 from pydantic import BaseModel
 
 from src.db_models.mongo_base_model import MongoBaseModel
-from src.models.review import ReviewContent
 
 
 class ReviewDocument(BaseModel):
@@ -23,10 +22,10 @@ class ReviewModel(MongoBaseModel[ReviewDocument]):
     def __init__(self, database: AgnosticDatabase):
         super().__init__(database, 'reviews', ReviewDocument.from_mongo)
 
-    async def add_review(self, user_id: str, movie_id: str, review: ReviewContent) -> ReviewDocument:
-        insert_dict = {'user_id': user_id, 'movie_id': movie_id, 'review': review.review}
+    async def add_review(self, user_id: str, movie_id: str, review: str) -> ReviewDocument:
+        insert_dict = {'user_id': user_id, 'movie_id': movie_id, 'review': review}
         inserted = await self.collection.insert_one(insert_dict)
-        return ReviewDocument(id=inserted.inserted_id, user_id=user_id, movie_id=movie_id, review=review.review)
+        return ReviewDocument(id=inserted.inserted_id, user_id=user_id, movie_id=movie_id, review=review)
 
     async def remove_review(self, review_id: str) -> None:
         await self.collection.delete_one({'_id': review_id})  # Assuming there's an 'id' field
@@ -34,10 +33,10 @@ class ReviewModel(MongoBaseModel[ReviewDocument]):
     async def get_reviews_by_movie(self, movie_id: str) -> List[ReviewDocument]:
         return await self.find({'movie_id': movie_id})
 
-    async def update_review(self, review_id: str, user_id: str, new_review_content: ReviewContent) -> bool:
+    async def update_review(self, review_id: str, user_id: str, new_review_content: str) -> bool:
         update_result = await self.collection.update_one(
-            {'_id': review_id, 'user_id': user_id},
-            {'$set': {'review': new_review_content.review}},
+            {'_id': review_id},
+            {'$set': {'review': new_review_content}},
         )
 
         return update_result.modified_count != 0
